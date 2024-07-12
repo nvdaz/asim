5c41affb06e997c8fcadfa190e0a81667c2e8f47
KY233466 <zihan.yang@tufts.edu>
2024-07-12 22:55:30 +0800

    add collape choices

--- frontend/src/components/InputSection/choice.js
+++ frontend/src/components/InputSection/choice.js
@@ -1,7 +1,9 @@
 import { useState, useRef, useEffect, forwardRef } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import styles from "./index.module.css";
 
 const ChoicesSection = ({ options, handleButtonClick }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const [maxWidth, setMaxWidth] = useState(0);
   const elementsRef = useRef([]);
 
@@ -12,20 +14,35 @@ const ChoicesSection = ({ options, handleButtonClick }) => {
 
   return (
     <div className={styles.choicesWrapper}>
-      <div>Choose an option:</div>
-      <div className={styles.choices}>
-        {Object.keys(options).map((index) => {
-          return (
-            <Choice
-              width={maxWidth - 25}
-              ref={(rel) => (elementsRef.current[index] = rel)}
-              key={index}
-              message={options[index]}
-              func={() => handleButtonClick(index, options[index])}
-            />
-          );
-        })}
+      <div
+        className={styles.choicesTitlesWrapper}
+        style={{
+          paddingBottom: isCollapsed ? "0px" : "5px",
+          paddingTop: isCollapsed ? "0px" : "15px",
+          transform: isCollapsed ? "translate(0px, 10px)" : "translate(0px)",
+        }}
+      >
+        {isCollapsed ? <div></div> : <div>Choose an option:</div>}
+        <KeyboardArrowDownIcon
+          className={isCollapsed ? styles.arrowRotate : styles.arrow}
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        />
       </div>
+      {!isCollapsed && (
+        <div className={styles.choices}>
+          {Object.keys(options).map((index) => {
+            return (
+              <Choice
+                width={maxWidth - 25}
+                ref={(rel) => (elementsRef.current[index] = rel)}
+                key={index}
+                message={options[index]}
+                func={() => handleButtonClick(index, options[index])}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
