ca2a81f64dcc90930e6ec056a24fdb10c1692e77
Kyle Wigdor <37520732+nvdaz@users.noreply.github.com>
2024-09-26 19:12:52 -0400

    config db

--- api/api/db/client.py
+++ api/api/db/client.py
@@ -3,8 +3,9 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_URI = os.getenv("MONGO_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "autsim")
 
 
 client = AsyncIOMotorClient(MONGO_URI, uuidRepresentation="standard")
 
-db = client.autsim
+db = client[DATABASE_NAME]
